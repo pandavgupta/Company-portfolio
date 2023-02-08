9e1c04e3dccf0337c9091f4fa5beaeacd34666aa
sayan <sayanmanna04@gmail.com>
2023-02-08 16:23:53 +0530

    carousel developing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav'
+import Carousel from './components/Carousel'
 
 function App() {
   return (
     <>
     <Nav/>
+    <Carousel/>
     <div>body</div>
     <div>footer</div>
     </>
---
+++ src/components/Carousel.js
@@ -0,0 +1,6 @@
+function Carousel(){
+    return (
+        <div></div>
+    );
+}
+export default Carousel;
