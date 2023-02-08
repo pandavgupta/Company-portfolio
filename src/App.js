eb706be0e073050d66e6f29157b76a7e56669436
pandav gupta <pandavgupta10@gmail.com>
2023-02-08 15:17:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav'
+
+function App() {
+  return (
+    <>
+    <Nav/>
+    <div>body</div>
+    <div>footer</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Nav.css'
+function Nav(){
+    return (
+        <div className='Nav-bar'>
+            <div>logo</div>
+            <div>home</div>
+            <div>about us</div>
+            <div>contact</div>
+        </div>
+    );
+}
+export default Nav;
