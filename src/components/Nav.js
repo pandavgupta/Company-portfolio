dbf39f4e05ae4e302068028eede94e074d08cd17
sayan <sayanmanna04@gmail.com>
2023-02-08 15:54:44 +0530

    nav bar done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Nav.css'
+import logo from '../Images/logo.svg'
 function Nav(){
     return (
-        <div className='Nav-bar'>
-            <div>logo</div>
-            <div>home</div>
-            <div>about us</div>
-            <div>contact</div>
+        <div className="navbar">
+            <header>
+                <img src={logo} alt="logo" className="logo"/>
+                <nav>
+                    <ul className="nav-links">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Projects</a></li>
+                        <li><a href="#">Blogs</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </nav>
+            </header>
         </div>
     );
 }
